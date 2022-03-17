ee21da6254173161565c02b16b9d959a9f6d9d1e
omkar-7888 <101782885+omkar-7888@users.noreply.github.com>
2022-03-17 09:31:39 +0530

    This First Commit

---
+++ CharPattern.java
@@ -0,0 +1,15 @@
+class CharPattern 
+{
+	public static void main(String[] args) 
+	{
+		int a='a';
+		for (int i=1;i<=5 ;i++ )
+		{
+			for (int j=1;j<=5 ;j++ )
+			{
+				System.out.print(a+" ");
+			}
+			System.out.println();
+		}
+	}
+}
